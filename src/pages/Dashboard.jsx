aa1a2bd82678540a092548d5d960dbc0076293bf
omidrayaneh <omidrayaneh@gmail.com>
2023-01-22 11:31:18 +0330

    fix dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-
-function Dashboard(props) {
-    return (
-        <div>
-            Dashboard
+import React from "react";
+import DashboardStatusBox from "../components/DashboardStatusBox";
+import TransactionChart from "../components/TransactionChart";
+import BuyerChart from "../components/BuyerChart";
+const Dashboard =() => {
+  return (
+      <div className="flex flex-col gap-4">
+        <DashboardStatusBox/>
+        <div className="flex flex-row gap-4 w-full">
+            <TransactionChart/>
+            <BuyerChart/>
         </div>
-    );
+      </div>
+  );
 }
 
 export default Dashboard;
